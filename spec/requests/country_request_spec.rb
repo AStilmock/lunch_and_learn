7812b71fd723374f9cd4bd0371b65763a63e65db
Andrew Stilmock <andrew.stilmock@gmail.com>
2023-08-05 23:26:09 -0600

    Expose Country API test updates

---
+++ spec/requests/country_request_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe 'Countries API' do
+  describe 'api request response path' do
+    it 'successful request' do
+      json_response = File.read('spec/fixtures/countries/france_search.json')
+      stub_request(:get, "https://restcountries.com/v3.1/name/France?fullText=true")
+      .to_return(status: 200, body: json_response)
+      get '/api/v1/countries', params: {name: 'france'}, headers: { 'Accept' => 'application/json' }
+      require 'pry'; binding.pry
+      expect(response).to be_successful
+      expect(response.status).to eq(204)
+    end
+  end
+
+  it 'gets a country name'  do
+    json_response = File.read('spec/fixtures/countries/france_search.json')
+    stub_request(:get, "https://restcountries.com/v3.1/name/france?fullText=true")
+    .to_return(status: 200, body: json_response)
+    get '/api/v1/countries', params: {name: 'france'}, headers: { 'Accept' => 'application/json' }
+    country = JSON.parse(response.body, symbolize_names: true)
+  end
+end
