00b3d013eced028fbe1dae4a56a88ca29ac718f3
Andrew Stilmock <andrew.stilmock@gmail.com>
2023-08-06 16:11:44 -0600

    Country API expose endpoint test updates

--- spec/facades/country_search_spec.rb
+++ spec/facades/country_search_spec.rb
@@ -4,10 +4,9 @@ RSpec.describe CountrySearch do
   describe 'country search facade' do
     it 'returns a country' do
       json_response = File.read('spec/fixtures/countries/france_search.json')
-      stub_request(:get, "https://restcountries.com/name/france?fullText=true").
+      stub_request(:get, "https://restcountries.com/v3.1/all").
         to_return(status: 200, body: json_response)
-      @country = CountrySearch.new.get_country("france")
-
+      @country = CountrySearch.new.get_country
       expect(@country).to be_a Country
       expect(@country.name).to eq "France"
     end
--- spec/services/country_service_spec.rb
+++ spec/services/country_service_spec.rb
@@ -4,9 +4,9 @@ RSpec.describe CountryService do
   describe 'countries api request response' do
     it 'returns a response' do
       json_response = File.read('spec/fixtures/countries/france_search.json')
-      stub_request(:get, "https://restcountries.com/name/france?fullText=true").
+      stub_request(:get, "https://restcountries.com/v3.1/all").
         to_return(status: 200, body: json_response)
-      country_data = CountryService.new.get_country("france").first
+      country_data = CountryService.new.get_country
       @country = country_data[:name][:common]
 
       expect(@country).to be_a String
