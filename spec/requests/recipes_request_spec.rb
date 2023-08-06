e83ec1f56940d267b01deb85b79661573caf71fe
Andrew Stilmock <andrew.stilmock@gmail.com>
2023-08-05 21:15:51 -0600

    expose recipes api request test setup

--- spec/requests/recipes_request_spec.rb
+++ spec/requests/recipes_request_spec.rb
@@ -3,10 +3,15 @@ require 'rails_helper'
 RSpec.describe 'Recipes API' do
   describe 'api request response path' do
     it 'successful request' do
-      get "api/v1/recipes?search=france"
-
-      expect(request).to be_successful
-      expect(request.status).to eq(200)
+      json_response = File.read('spec/fixtures/recipes/france_recipes.json')
+      stub_request(:get, "https://api.edamam.com/api/recipes/v2?app_id=aefdc154&app_key=e1630b1eb84bc7c2a45d0ad75126bc2e&q=france&type=public")
+      .to_return(status: 200, body: json_response)
+      get '/api/v1/recipes/france'
+      recipes = JSON.parse(response.body, symbolize_names: true)
+      expect(response).to be_successful
+      expect(response.status).to eq(200)
     end
+
+    it ''
   end
 end
