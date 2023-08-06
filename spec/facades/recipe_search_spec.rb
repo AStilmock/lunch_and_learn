3403c6028e2058fc612c5b77b2934043a020b496
Andrew Stilmock <andrew.stilmock@gmail.com>
2023-08-05 20:04:39 -0600

    initial api test updates

--- spec/facades/recipe_search_spec.rb
+++ spec/facades/recipe_search_spec.rb
@@ -7,8 +7,22 @@ RSpec.describe RecipeSearch do
       stub_request(:get, "https://api.edamam.com/api/recipes/v2?type=public&app_id=aefdc154&app_key=e1630b1eb84bc7c2a45d0ad75126bc2e&q=france").
         to_return(status: 200, body: json_response)
       response = Faraday.get("https://api.edamam.com/api/recipes/v2?type=public&app_id=aefdc154&app_key=e1630b1eb84bc7c2a45d0ad75126bc2e&q=france")
-      data = JSON.parse(response.body, symbolize_names: true)
-      require 'pry'; binding.pry
+      data = JSON.parse(response.body, symbolize_names: true)[:hits]
+      @recipes = data.map do |r|
+        Recipe.new(r, "france")
+      end
+
+      expect(@recipes).to be_an(Array)
+      expect(@recipes.count).to eq(20)
+      @recipes.each do |r|
+        expect(r).to be_a Recipe 
+        expect(r.id).to eq nil
+        expect(r.type).to be_a(String)
+        expect(r.title).to be_a(String)
+        expect(r.url).to be_a(String)
+        expect(r.country).to eq "france"
+        expect(r.image).to be_a(String)
+      end
     end
   end
 end
--- spec/services/recipe_service_spec.rb
+++ spec/services/recipe_service_spec.rb
@@ -9,24 +9,23 @@ RSpec.describe RecipeService do
     end
 
     it 'returns a response' do
-      # json_response = File.read('spec/fixtures/recipes/france_recipes.json')
-      # stub_request(:get, "https://api.edamam.com/api/recipes/v2?type=public&app_id=aefdc154&app_key=e1630b1eb84bc7c2a45d0ad75126bc2e&q=france").
-      #   to_return(status: 200, body: json_response)
-      # response = Faraday.get("https://api.edamam.com/api/recipes/v2?type=public&app_id=aefdc154&app_key=e1630b1eb84bc7c2a45d0ad75126bc2e&q=france")
-      # data = JSON.parse(response.body, symbolize_names: true)
-      recipe_data = RecipeService.new.recipes_by_country("france")
-      require 'pry'; binding.pry
-
-      expect(@recipes).to be_an(Array)
-      expect(@recipes.count).to eq(20)
-      @recipes.each do |r|
-        expect(r).to be_a(Recipe)
-        expect(r.country).to be_a(String)
-        expect(r.id).to be_nil
-        expect(r.image).to be_a(String)
-        expect(r.title).to be_a(String)
-        expect(r.type).to be_a(String)
-        expect(r.url).to be_a(String)
+      json_response = File.read('spec/fixtures/recipes/france_recipes.json')
+      stub_request(:get, "https://api.edamam.com/api/recipes/v2?type=public&app_id=aefdc154&app_key=e1630b1eb84bc7c2a45d0ad75126bc2e&q=france").
+        to_return(status: 200, body: json_response)
+      response = Faraday.get("https://api.edamam.com/api/recipes/v2?type=public&app_id=aefdc154&app_key=e1630b1eb84bc7c2a45d0ad75126bc2e&q=france")
+      data = JSON.parse(response.body, symbolize_names: true)[:hits]
+      
+      expect(data).to be_an(Array)
+      expect(data.count).to eq(20)
+      data.each do |d|
+        expect(d).to be_a(Hash)
+        expect(d).to have_key(:recipe)
+        expect(d[:recipe]).to be_a(Hash)
+        expect(d[:recipe][:label]).to be_a(String)
+        expect(d[:recipe][:image]).to be_a(String)
+        expect(d[:recipe][:url]).to be_a(String)
+        expect(d[:recipe]).to_not have_key(:id)
+        expect(d[:recipe]).to_not have_key(:type)
       end
     end
   end
