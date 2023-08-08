3adda08de7c0c955c2df58701c8d973c22a144db
Andrew Stilmock <andrew.stilmock@gmail.com>
2023-08-07 19:20:44 -0600

    Learning resource test upates - tests passing

--- spec/poros/image_spec.rb
+++ spec/poros/image_spec.rb
@@ -4,14 +4,14 @@ RSpec.describe "Image PORO" do
   describe 'Image' do
     it 'exists and has attributes' do
       data = {
-        :alt => "MacBook Pro on top of brown table",
+        :alt_description => "MacBook Pro on top of brown table",
         :urls => {
           :raw => "https://images.unsplash.com/photo-1496181133206-80ce9b88a853?ixid=M3w0ODU5ODR8MHwxfHNlYXJjaHwxfHxjb21wdXRlcnxlbnwwfHx8fDE2OTE0NDk3MjN8MA&ixlib=rb-4.0.3"
         }
       }
       image = Image.new(data)
       expect(image).to be_a Image
-      expect(image.alt_tag).to eq(data[:alt])
+      expect(image.alt_tag).to eq(data[:alt_description])
       expect(image.alt_tag).to eq "MacBook Pro on top of brown table"
       expect(image.url).to eq(data[:urls][:raw])
       expect(image.url).to eq "https://images.unsplash.com/photo-1496181133206-80ce9b88a853?ixid=M3w0ODU5ODR8MHwxfHNlYXJjaHwxfHxjb21wdXRlcnxlbnwwfHx8fDE2OTE0NDk3MjN8MA&ixlib=rb-4.0.3"
--- spec/services/image_service_spec.rb
+++ spec/services/image_service_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe "Image Service" do
       data = File.read('spec/fixtures/images/images.json')
       stub_request(:get, "https://api.unsplash.com/search/photos?client_id=#{ENV['UNSPLASHKEY']}&query=computer")
       .to_return(status: 200, body: data)
-      image = ImageService.new.get_photos("computer")
+      image = ImageService.new.get_images("computer")
 
       expect(image).to be_a Hash
       expect(image).to have_key :results
@@ -19,12 +19,12 @@ RSpec.describe "Image Service" do
 
     it 'gets values from keys' do
       data = File.read('spec/fixtures/images/images.json')
-      stub_request(:get, "https://api.unsplash.com/search/photos?client_id=#{ENV['UNSPLASHKEY']}&query=computer")
+      stub_request(:get, "https://api.unsplash.com/search/photos?client_id=#{ENV['UNSPLASHKEY']}&query=france")
       .to_return(status: 200, body: data)
-      image = ImageService.new.get_photos("computer")
-      expect(image[:results].first[:alt_description]).to eq "MacBook Pro on top of brown table"
+      image = ImageService.new.get_images("france")
+      expect(image[:results].first[:alt_description]).to eq "group of people walking near body of water and building under blue sky at daytime"
       expect(image[:results].first[:urls]).to be_a Hash
-      expect(image[:results].first[:urls][:raw]).to eq "https://images.unsplash.com/photo-1496181133206-80ce9b88a853?ixid=M3w0ODU5ODR8MHwxfHNlYXJjaHwxfHxjb21wdXRlcnxlbnwwfHx8fDE2OTE0NDk3MjN8MA&ixlib=rb-4.0.3"
+      expect(image[:results].first[:urls][:raw]).to eq "https://images.unsplash.com/photo-1503917988258-f87a78e3c995?ixid=M3w0ODU5ODR8MHwxfHNlYXJjaHwxfHxmcmFuY2V8ZW58MHx8fHwxNjkxNDU1ODM3fDA&ixlib=rb-4.0.3"
     end
   end
 end
