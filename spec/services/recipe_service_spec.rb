8e5c6eba880c46eae64a59178e99edbc57e4eb8f
Andrew Stilmock <andrew.stilmock@gmail.com>
2023-08-07 19:37:05 -0600

    hide api key in test

--- spec/services/recipe_service_spec.rb
+++ spec/services/recipe_service_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe RecipeService do
   describe 'recipe api request response' do
     it 'returns a response' do
       json_response = File.read('spec/fixtures/recipes/france_recipes.json')
-      stub_request(:get, "https://api.edamam.com/api/recipes/v2?app_id=aefdc154&app_key=e1630b1eb84bc7c2a45d0ad75126bc2e&q=france&type=public").
+      stub_request(:get, "https://api.edamam.com/api/recipes/v2?app_id=#{ENV['recID']}4&app_key=#{ENV['recikey']}&q=france&type=public").
         to_return(status: 200, body: json_response)
       @recipes = RecipeService.new.recipes_by_country("france")[:hits]
       
