d8be8cdd76fef29cabf00526edf71b535d0c1681
Andrew Stilmock <andrew.stilmock@gmail.com>
2023-08-08 18:55:31 -0600

    user favorites path tests complete

--- spec/requests/favorites_request_spec.rb
+++ spec/requests/favorites_request_spec.rb
@@ -20,11 +20,19 @@ RSpec.describe 'Save favorite recipes' do
     it 'can get user favorites' do
       user = User.create!(name: "email15", email: "email5@email.com", password: "password", api_key: "apikey1234567890")
       fav1 = user.favorites.create!(country: "france", recipe_link: "http://www.edamam.com/ontologies/edamam.owl#recipe_48df011ddfee5cd4dacff1463e8701b0", recipe_title: "Herb And White Wine Granita")
-      fav1 = user.favorites.create!(country: "france", recipe_link: "http://www.edamam.com/ontologies/edamam.owl#recipe_0d543f253608386e21a3c9a62a4e43e7", recipe_title: "Savory Sesame Cookies")
-      fav1 = user.favorites.create!(country: "france", recipe_link: "http://www.edamam.com/ontologies/edamam.owl#recipe_8f9e0a801e673792608e1a8b08b587d9", recipe_title: "France Meets Italy Panini")
+      fav2 = user.favorites.create!(country: "france", recipe_link: "http://www.edamam.com/ontologies/edamam.owl#recipe_0d543f253608386e21a3c9a62a4e43e7", recipe_title: "Savory Sesame Cookies")
+      fav3 = user.favorites.create!(country: "france", recipe_link: "http://www.edamam.com/ontologies/edamam.owl#recipe_8f9e0a801e673792608e1a8b08b587d9", recipe_title: "France Meets Italy Panini")
       get '/api/v1/favorites?api_key=apikey1234567890'
-      response_info = JSON.parse(response.body, symbolize_names: true)
-      require 'pry'; binding.pry
+      favorites = JSON.parse(response.body, symbolize_names: true)[:data]
+      expect(favorites).to be_a Array
+      expect(favorites.count).to eq 3
+      expect(favorites.first[:id]).to eq fav1.id.to_s
+      expect(favorites.first[:type]).to eq "favorite"
+      expect(favorites.second).to have_key :attributes
+      expect(favorites.second[:attributes][:recipe_title]).to eq fav2.recipe_title
+      expect(favorites.second[:attributes][:recipe_link]).to eq fav2.recipe_link
+      expect(favorites.second[:attributes][:country]).to eq fav3.country
+      expect(favorites.third[:attributes][:created_at]).to eq fav3.created_at.strftime("%Y-%m-%dT%H:%M:%S.%LZ")
     end
   end
 end
